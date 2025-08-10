b46802e768169fbeca95f2fb8e0803ee37e5e2f4
bsm2709 <bmeti2709@example.com>
2025-08-11 00:04:11 +0530

    ADD in views.py

--- models_to_frontend_07/characters/views.py
+++ models_to_frontend_07/characters/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render  
 from .models import Characters
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def characters(request):
     charac = Characters.objects.all()
     return render(request,"characters/characters.html", {'characters': charac})
+
+
+def characterdetails(request, char_id):
+    character = get_object_or_404(Characters, pk = char_id)
+    return render()
