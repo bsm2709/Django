df987b415becafb0063b6b644906ecc71ab75f50
bsm2709 <bmeti2709@example.com>
2025-08-10 13:53:29 +0530

    Handle models and settings related

--- models_migrations_06/card/admin.py
+++ models_migrations_06/card/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from . models import card_varities
 
 # Register your models here.
+admin.site.register(card_varities)
--- models_migrations_06/card/models.py
+++ models_migrations_06/card/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class card_varities(models.Model):
+    CARD_TYPES = [
+        ('MC', 'Marage Card'),
+        ('RC', 'Reception Card'),
+        ('DC', 'Death Card'),
+        ('BC', 'Birthday Card'),
+        ('BS', 'Baby Shower Card'),
+    ]
+
+    name = models.CharField(max_length= 50)
+    image = models.ImageField(upload_to= 'card/')
+    date_addded= models.DateTimeField(default=timezone.now)
+    types = models.CharField(max_length=2, choices=CARD_TYPES)
+    description = models.TextField('')
+    cost = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    
